cb4ec5dd9e426e0cbc15126ae21f620a087ffda1
Bugra Bilin <bugra.bilin@cern.ch>
2024-07-23 14:38:52 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 # install frontend dependencies
-cd client && npm install  --force && npm install  --force --save history
+cd client 
+echo "I managed to go to client folder"
+npm install --force 
+echo "I managed install"
+npm install --force --save history
+echo "I managed save history"
 # compile frontend files
-npm run compile --force && cd ..
+npm run compile --force 
+echo "I managed compile"
+cd ..
 
 # create logs directory if doesn't exist
 mkdir -p logs
