602d8b5c997b30495fb3dc980926487236a8bb9b
Bugra Bilin <bugra.bilin@cern.ch>
2024-07-23 14:42:11 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 # install frontend dependencies
-cd client 
-echo "I managed to go to client folder"
-npm install --force 
-echo "I managed install"
-npm install --force --save history
-echo "I managed save history"
-# compile frontend files
-npm run compile --force 
-echo "I managed compile"
-cd ..
+#cd client 
+#echo "I managed to go to client folder"
+#npm install --force 
+#echo "I managed install"
+#npm install --force --save history
+#echo "I managed save history"
+## compile frontend files
+#npm run compile --force 
+#echo "I managed compile"
+#cd ..
 
 # create logs directory if doesn't exist
 mkdir -p logs
